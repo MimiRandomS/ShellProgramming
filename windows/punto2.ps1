dd89844686d56f2d3ffa02c8bad1ce7ffde8d884
MimiRandomS <geronimo0scorpion@gmail.com>
2024-10-13 17:14:59 -0500

    Correccion punto2.ps1 con la ruta especificada y agregando el archivo de los enunciados de los ejercicios.

--- windows/punto2.ps1
+++ windows/punto2.ps1
@@ -1,4 +1,4 @@
 Clear-Host
-$path = "C:\prueba\polin.txt"
+$path = "C:\Windows\System32\drivers\etc\hosts"
 $lineas = (Get-Content -Path $path).Count
 Write-Host "El numero de lineas del archivo $path es: $lineas"
