e1b2832120826d5f9c01b84e2886efc8fd6e3241
MimiRandomS <geronimo0scorpion@gmail.com>
2024-10-25 10:57:16 -0500

    Update punto7b.sh
    Correccion punto 7b

--- unix/punto7b.sh
+++ unix/punto7b.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-LOG_FILE="/var/log/authlog"
-
-if [ ! -f "$LOG_FILE" ]; then
-    echo "Log file '$LOG_FILE' not found!"
-    exit 1
-fi
-
-echo "Checking for failed login attempts for user root..."
-failed_attempts=$(grep "LOGIN FAILURE" "$LOG_FILE" | grep "root")
-
-if [ -z "$failed_attempts" ]; then
-    echo "No failed login attempts for user root found."
-    exit 0
-fi
-
-echo "Failed login attempts for user root:"
-grep "FAILED LOGIN" "$LOG_FILE" | grep "root" | awk '{print $1, $2, $3, $NF}'
-
-total_attempts=$(echo "$failed_attempts" | wc -l)
-echo "Total failed login attempts for root: $total_attempts"
+grep -i "failure" /var/log/authlog | cut -d: -f 1,2,3
+total=$(grep -i "failure" /var/log/authlog | wc -l)
+echo "Numero de intentos fallidos: $total"
